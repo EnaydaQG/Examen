661254903c8025926a7be1cf32964f055fc384e1
eny777ceci <eniyuoki777@hotmail.com>
2018-08-03 23:09:02 -0700

    1

--- src/main/java/holaMundo.java
+++ src/main/java/holaMundo.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class holaMundo {
+    public static void main(String[] args) {
+        System.out.println("<<<< Hola >>>>”");
+        System.out.println("<<<< Mundo >>>>”");
+    }
 }
